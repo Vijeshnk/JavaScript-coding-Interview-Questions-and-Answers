7b193933c51d35dc9bbac0771cd6db52725e9122
Vijeshnk <vijeshnirala@gmail.com>
2024-02-24 23:12:35 +0530

    update in isprime

--- isprime.js
+++ isprime.js
@@ -1,5 +1,17 @@
 const isPrime=(n)=>{
-    if(n%2 ===0){
-        r
+    if(n<2 || !Number.isInteger(n)){
+        return false
     }
+    for(let i=2;i<= Math.sqrt(n);i++){
+        if(n%i ===0){
+            return false
+        }
+    }
+    return true
 }
+
+console.log(isPrime(6));
+console.log(isPrime(2)); // true
+console.log(isPrime(11)); // true
+console.log(isPrime(4)); // false
+console.log(isPrime(18)); 
