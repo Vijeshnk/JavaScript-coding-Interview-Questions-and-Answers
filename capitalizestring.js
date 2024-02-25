c27eb3c4315ee4c14730eb93dd11bf7d5b9ac7c6
Vijeshnk <vijeshnirala@gmail.com>
2024-02-25 19:35:49 +0530

    update in capitalize the string

--- capitalizestring.js
+++ capitalizestring.js
@@ -1,13 +1,9 @@
-const capitalize =(str)=>{
-
-    // return str[0].toUpperCase()
-   let first = str[0].toUpperCase()
-   let last = str.slice(1)
-
-   let result = first +last
-   return result
-
-
+const capitalize = (str) => {
+    return str
+    .toLowerCase()
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ')
 }
 
-console.log(capitalize('bye'))
+console.log(capitalize('javaScript is a fun programming language')) //Javascript Is A Fun Programming Language
